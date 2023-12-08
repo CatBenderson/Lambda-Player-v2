5d5f09348f816a2e18402a5f189cae7f0f747351
CatBenderson <ameyalli030@gmail.com>
2023-12-08 07:18:49 -0600

    Cambios menores

--- src/ModalComandos.js
+++ src/ModalComandos.js
@@ -12,15 +12,15 @@ export default function ModalComandos() {
                     <ModalHeader>Comandos de voz</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                        <Text m={2}><b>"Reproducir" - </b> Quita la pausa </Text>
-                        <Text m={2}><b>"Detener" - </b> Poner pausa </Text>
-                        <Text m={2}><b>"Número de canción - "</b> Reproduce la canción con el número dicho</Text>
+                        <Text m={2}><b>"Reproducir" - </b> Reproduce la canción actual </Text>
+                        <Text m={2}><b>"Detener" - </b> Pausa la cancion en reproducción </Text>
+                        <Text m={2}><b>"Número de canción" - </b> Reproduce la canción con el número dicho</Text>
                         <Text m={2}><b>"Anterior" - </b>  Reproduce la canción anterior  </Text>
                         <Text m={2}><b>"Siguiente" - </b> Reproduce la siguiente canción </Text>
-                        <Text m={2}><b>"Comenzar de nuevo" - </b> Reinciar canción actual </Text>
-                        <Text m={2}><b>"Silenciar" - </b> Bajar volumen a 0 </Text>
-                        <Text m={2}><b>"Bajar" - </b> Bajar volumen -10 </Text>
-                        <Text m={2}><b>"Subir" - </b> Subir volumen +10 </Text>
+                        <Text m={2}><b>"Comenzar de nuevo" - </b> Reinicia la canción actual </Text>
+                        <Text m={2}><b>"Silenciar" - </b> Baja el volumen a 0 </Text>
+                        <Text m={2}><b>"Bajar" - </b> Baja el volumen -10 </Text>
+                        <Text m={2}><b>"Subir" - </b> Sube el volumen +10 </Text>
                     </ModalBody>
                     <ModalFooter>
                         <Button backgroundColor="#ED4937" color="white" mr={3} onClick={onClose}>
